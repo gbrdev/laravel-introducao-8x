ab6915a5fac17c3af5c9e8fd9fc3e673d767b282
jlgregorio81 <jlgregorio81.gmail.com>
2020-11-01 23:07:16 -0300

    Second Commit - Database

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,19 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
-use stdClass;
 
 class ClientController extends Controller
 {
-
-    public function __construct()
-    {
-        if (!session('clients')) {
-            session(['clients' => $this->createClients()]);
-        }
-    }
-
     /**
      * Display a listing of the resource.
      *
@@ -22,7 +14,8 @@ class ClientController extends Controller
      */
     public function index()
     {
-        return view('client.index')->with('clients', session('clients'));
+        $clients = Client::all();
+        return view('client.index')->with('clients', $clients);
     }
 
     /**
@@ -43,27 +36,11 @@ class ClientController extends Controller
      */
     public function store(Request $request)
     {
-        
-        $rules = [
-            'name' => 'required|max:20',
-            'age' => 'required|numeric|between:18,50'
-        ];
-
-        $request->validate($rules);
-        
-        $clients = session('clients');
-        $lastId = 0;
-        if($clients){
-            $lastIndex = count($clients) - 1;
-            $lastId = $clients[$lastIndex]->id;
-        }
-        $c = new stdClass;
-        $c->id = $lastId + 1;
-        $c->name = $request->input('name');
-        $c->age = $request->input('age');
-        $clients[] = $c;
-        session(['clients' => $clients]);
-        return view('client.index')->with('clients', $clients);
+        $client = new Client();
+        $client->name = $request->input('name');
+        $client->age = $request->input('age');
+        $client->save();
+        return redirect(route('client.index'));
     }
 
     /**
@@ -74,13 +51,8 @@ class ClientController extends Controller
      */
     public function show($id)
     {
-        $clients = session('clients');
-        $obj = $this->arrayFind($clients, $id);
-        if($obj){
-            return view('client.show')->with('client', $obj);
-        } else {
-            abort(404);
-        }
+        $client = Client::find($id);
+        return view('client.show')->with('client', $client);
     }
 
     /**
@@ -91,9 +63,8 @@ class ClientController extends Controller
      */
     public function edit($id)
     {
-        $clients = session('clients');
-        $c = $this->arrayFind($clients, $id);
-        return view('client.update')->with('client', $c);
+        $client = Client::find($id);
+        return view('client.update')->with('client', $client);    
     }
 
     /**
@@ -105,14 +76,11 @@ class ClientController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $clients = session('clients');
-        $i = $this->arraySearch($clients, 'id', $id);
-        $clients[$i]->name = $request->input('name');
-        $clients[$i]->age = $request->input('age');
-
-        session(['clients' => $clients]);
-        return view('client.index')->with('clients', $clients);
-
+        $client = Client::find($id);
+        $client->name = $request->input('name');
+        $client->age = $request->input('age');
+        $client->save();
+        return redirect(route('client.index'));
     }
 
     /**
@@ -123,57 +91,7 @@ class ClientController extends Controller
      */
     public function destroy($id)
     {
-        $clients = session('clients');
-        $i = $this->arraySearch($clients, 'id', $id);        
-        if($i >= 0){
-            unset($clients[$i]);
-            $clients = array_values($clients);
-            session(['clients' => $clients]);
-
-        }
-        return view('client.index')->with('clients', session('clients'));
+        Client::destroy($id);
+        return redirect(route('client.index'));
     }
-
-    public function createClients()
-    {
-        $clients = [];
-
-        $client = new stdClass;
-        $client->id = 1;
-        $client->name = 'Luke Skywalker';
-        $client->age = 40;
-        $clients[] = $client;
-
-        $client = new stdClass;
-        $client->id = 2;
-        $client->name = 'Lea Organa';
-        $client->age = 40;
-        $clients[] = $client;
-
-        return $clients;
-    }
-
-    private function arrayFind(array $array, int $id)
-    {
-        if ($array) {
-            foreach ($array as $obj) {
-                if ($obj->id == $id)
-                    return $obj;
-            }
-            return null;
-        }
-    }
-
-    private function arraySearch(array $array, string $key, int $search)
-    {
-        if($array){
-            for($i = 0; $i <= count($array) - 1; $i++){
-                if($array[$i]->$key == $search){
-                    return $i;
-                }
-            }
-            return -1;
-        }
-    }
-
 }
